17736d196c6855a2e37975334d5391498c8b8721
Phuong Nam <phuongnam2905@gmail.com>
2025-10-01 22:30:47 +0700

    feat: Alice changes greeting

--- src/main/java/com/example/identity_service/controllers/HelloController.java
+++ src/main/java/com/example/identity_service/controllers/HelloController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @GetMapping("/hello")
     public String hello() {
-        return "Hello from feature/hello-api";
+        return "Hello from Alice";
     }
 }
 
